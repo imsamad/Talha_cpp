0f8eab26e3ff5f62824a74713690dac54d4e1314
imsamad <imsamad00@gmail.com>
2023-11-29 22:30:08 +0530

    day one

---
+++ variable.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+// Entrypoint - run / start
+int main() {
+    
+    int x = 10;
+    
+    // Two parts.
+    // 1.) int x;
+    // 2.) x = 10;
+
+    // Instructions
+    int y = 20;
+
+    int sum = x + y + 5;
+
+    printf("%d ",sum);
+
+    return 0;
+}
+// exit
